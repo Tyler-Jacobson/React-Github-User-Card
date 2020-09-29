00cd5a2374d0e2d597f34c417444380b1c5dba24
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-09-29 15:14:09 -0700

    MVP Met

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Card from './Card'
+import Friend from './Friend'
+
+const userData = {}
+const userFollowers = []
+
+const userName = "tyler-jacobson"
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: userData,
+      userFollowers: userFollowers,
+    }
+  }
+
+  componentDidMount() {
+    console.log("Mounting")
+    axios.get(`https://api.github.com/users/${userName}`)
+    .then(res => {
+      this.setState({
+        userData: res.data
+      })
+    })
+    .catch(err => {
+      console.log("Catch", err)
+      debugger
+    })
+
+    axios.get(`https://api.github.com/users/${userName}/followers`)
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        userFollowers: res.data
+      })
+    })
+    .catch(err => {
+      debugger
+    })
+  }
+
+
+  render() {
+    console.log("Rendering")
+    return (
+      <div className="App-header">
+        <Card userData={this.state.userData} />
+        <div className="followersContainer">
+          <h2>Followers</h2>
+          {
+            this.state.userFollowers.map(follower => (
+              <Friend follower={follower} />
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
 }
+
+
 
 export default App;
